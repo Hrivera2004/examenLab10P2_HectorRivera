1ec50dbfebd079ad724ff534c8864de51f3eb366
hector Rivera <hrolriverav@gmail.com>
2024-03-22 14:21:56 -0600

    si

---
+++ src/examenlab10p2_hectorrivera/AdminAutos.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab10p2_hectorrivera;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hriverav
+ */
+public class AdminAutos {
+    File file;
+    ArrayList<Auto> autos = new ArrayList();
+    
+    public AdminAutos(String file){
+        this.file = new File(file);
+    }
+    
+    public ArrayList<Auto> getAutos(){
+        return autos;
+    }
+    public void addAutos(Auto auto){
+         autos.add(auto);
+    }
+    
+    public void readFile(){
+        try{
+            autos = new ArrayList();
+            FileInputStream fs = new FileInputStream(file);
+            ObjectInputStream os = new ObjectInputStream(fs);
+            
+            try{
+                Auto carro = (Auto)os.readObject();
+                while (carro!=null) {
+                    autos.add(carro);
+                    carro = (Auto)os.readObject();
+                }
+            }catch(EOFException eof){
+            }
+            os.close();
+            fs.close();
+        }catch(Exception E){
+            
+        }
+    }
+    public void addtoFile(){
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            
+            for (Auto auto : autos) {
+                oos.writeObject(auto);
+            }
+            oos.close();
+            fos.close();
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ src/examenlab10p2_hectorrivera/Auto.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab10p2_hectorrivera;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hriverav
+ */
+public class Auto implements Serializable{
+    private String marca,Modelo;
+    private int velocidad;
+    
+    public Auto(String marca, String Modelo, int velocidad){
+        this.marca = marca;
+        this.Modelo = Modelo;;
+        this.velocidad  =velocidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" + "marca=" + marca + ", Modelo=" + Modelo + ", velocidad=" + velocidad + '}';
+    }
+    
+    
+}
